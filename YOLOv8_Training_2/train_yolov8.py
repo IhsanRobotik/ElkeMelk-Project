0b67b193be84154fcebfe10f617c0d79235b80ef
elena x <gabrielavoicila13@gmail.com>
2024-10-07 10:28:39 +0200

    Changes to YOLOv8 Training folders
    I have changed the folders for ease of cloning repository
    
    -Removed datasets
    -Created a folder of the python programs to train and live feed test YOLO trainings
    -Saved the datasets to an external HDD in case we need those

--- YOLOv8_Training_2/train_yolov8.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-
-model = YOLO('yolov8n.pt')
-
-# Train the model using the dataset.yaml with high-resolution images, for 100 epochs
-model.train(data='C:/Users/kukumav/Desktop/To-Train/dataset/dataset.yaml', epochs=100, imgsz=1024)
