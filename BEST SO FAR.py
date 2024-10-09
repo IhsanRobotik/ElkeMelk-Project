daf5a6b935b8b7add6d8270e1dc22c783af648e8
Bastiaan <bastiaansnoeij@gmail.com>
2024-10-09 14:17:12 +0200

    working code finaly

--- BEST SO FAR.py
+++ BEST SO FAR.py
@@ -8,13 +8,22 @@ import cv2
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address and port
-s.bind(("192.168.0.1", 5005))
+s.bind(("192.168.0.45", 5005))
 
 # Listen for incoming connections
 s.listen(5)
 # Accept a connection from a client
 clientsocket, address = s.accept()
 print(f"Connection from {address} has been established!")
+
+bottlecoordsX = 295.70
+bottlecoordsY = 170.70
+
+robotcoordsX = 247.25
+robotcoordsY = -487.20
+
+offsetX = (robotcoordsX) + (bottlecoordsY)
+offsetY = (robotcoordsY) + (bottlecoordsX)
 
 firstposX = 111.43
 firstposY = -500.54
@@ -30,12 +39,12 @@ def create_trackbars():
     cv.resizeWindow("TrackBars", 640, 240)
 
     # Create trackbars for adjusting HSV filter values
-    cv2.createTrackbar("Hue Min", "TrackBars", 60, 179, empty)
-    cv2.createTrackbar("Hue Max", "TrackBars", 112, 179, empty)
-    cv2.createTrackbar("Sat Min", "TrackBars", 7, 255, empty)
+    cv2.createTrackbar("Hue Min", "TrackBars", 29, 179, empty)
+    cv2.createTrackbar("Hue Max", "TrackBars", 142, 179, empty)
+    cv2.createTrackbar("Sat Min", "TrackBars", 8, 255, empty)
     cv2.createTrackbar("Sat Max", "TrackBars", 36, 255, empty)
-    cv2.createTrackbar("Val Min", "TrackBars", 25, 255, empty)
-    cv2.createTrackbar("Val Max", "TrackBars", 155, 255, empty)
+    cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty)
+    cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
 
 def get_trackbar_values():
     # Read the current values of the trackbars
@@ -63,7 +72,7 @@ def main():
     conversion_factor = known_width_mm / known_pixel_width
 
     # Initialize the video capture object
-    cap = cv.VideoCapture(1)  # Change to 0 for the default camera
+    cap = cv.VideoCapture(0)  # Change to 0 for the default camera
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
@@ -87,6 +96,7 @@ def main():
         # Undistort the frame
         new_camera_mtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
         undistorted_frame = cv.undistort(frame, mtx, dist, None, new_camera_mtx)
+
         # x,y,w,h = roi
         # undistorted_frame = undistorted_frame[y:y+h, x:x+w]
 
@@ -113,7 +123,7 @@ def main():
         # Detect circles
         rows = gray.shape[0]
         circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
-                                   param1=35, param2=25,
+                                   param1=15, param2=15,
                                    minRadius=35, maxRadius=40)
 
         # Draw the circles on the mask result and label them
@@ -138,37 +148,38 @@ def main():
                 # Put text with circle number and center in mm
                 text = f"{index + 1}: ({center_mm[0]:.2f}, {center_mm[1]:.2f}) mm"
                 cv.putText(imgResult, text, (center_pixels[0] - 30, center_pixels[1]), 
-                           cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                           cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
                 
 
                 deltaY = (-1) * center_mm[0]
                 deltaX = (-1) * center_mm[1]
 
-                pickupX =  deltaX + 524.18 + ((firstposX - array[0]) * (-1))
-                pickupY =  deltaY + -298.80 + ((firstposY - array[1]) * (-1))
+                pickupX =  deltaX + offsetX + ((firstposX - array[0]) * (-1))
+                pickupY =  deltaY + offsetY + ((firstposY - array[1]) * (-1))
 
                 msg = clientsocket.recv(1024)
                 
                 if not msg:  # If no message is received, break the loop
                     break
+                msg = (msg.decode("utf-8"))
+                print(msg)
 
-                # msg = (msg.decode("utf-8"))
-                # print(msg)
-                # if "trig" in msg:
-                #     formatted_string = "({0}, {1})".format(pickupX, pickupY)
-                #     message_to_send = formatted_string  # Coordinates to send
-                #     clientsocket.send(bytes(message_to_send, "ascii"))
-                #     print(f"Circle {index + 1} - Coordinate: ({center_mm[0]:.2f}, {center_mm[1]:.2f}), Robot Pick-Up Coordinate: ({pickupX:.2f}, {pickupY:.2f})")                  
+                if "trig" in msg:
+                    formatted_string = "({0}, {1})".format(pickupX, pickupY)
+                    message_to_send = formatted_string  # Coordinates to send
+                    clientsocket.send(bytes(message_to_send, "ascii"))
+                    print(f"Circle {index + 1} - Coordinate: ({center_mm[0]:.2f}, {center_mm[1]:.2f}), Robot Pick-Up Coordinate: ({pickupX:.2f}, {pickupY:.2f})")                  
 
-                # elif "p" in msg:
-                #     cleaned_msg = msg.replace("p", "")
-                #     print("this is cleaned msg", cleaned_msg)
-                #     array = ast.literal_eval(cleaned_msg)
-                #     array[0] = array[0] * 1000
-                #     array[1] = array[1] * 1000
+                elif "p" in msg:
+                    cleaned_msg = msg.replace("p", "")
+                    cleaned_msg = cleaned_msg.replace("trig", "")
+                    print("this is cleaned msg", cleaned_msg)
+                    array = ast.literal_eval(cleaned_msg)
+                    array[0] = array[0] * 1000
+                    array[1] = array[1] * 1000
 
-                # else: 
-                #     break
+                else: 
+                    break
 
         # Display the undistorted frame and mask result
         cv.imshow("Detected Circles on Mask", imgResult)
