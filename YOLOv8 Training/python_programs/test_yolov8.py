f64eaee6b1766f7189e9703ff73343007fdb26e0
Ihsan Fadilah <130231696+IhsanRobotik@users.noreply.github.com>
2024-10-10 00:06:52 +0200

    cleanup

--- YOLOv8 Training/python_programs/test_yolov8.py
+++
@@ -1,6 +0,0 @@
-# I used this to test whether I had YOLO, as I had some issues throughout
-
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")  # Load a pre-trained model
-model.info()  # Print model information
